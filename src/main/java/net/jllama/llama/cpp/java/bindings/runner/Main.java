57bd7a466ed15d50a9a8df66b076193cee1983e5
Welby Seely <welbyseely@gmail.com>
2023-11-24 20:02:34 -0500

    LlamaOpaqueContext -> LlamaContext

--- src/main/java/net/jllama/llama/cpp/java/bindings/runner/Main.java
+++ src/main/java/net/jllama/llama/cpp/java/bindings/runner/Main.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import net.jllama.llama.cpp.java.bindings.LlamaContextParams;
 import net.jllama.llama.cpp.java.bindings.LlamaCpp;
 import net.jllama.llama.cpp.java.bindings.LlamaCppManager;
+import net.jllama.llama.cpp.java.bindings.LlamaLogLevel;
 import net.jllama.llama.cpp.java.bindings.LlamaModelParams;
 import net.jllama.llama.cpp.java.bindings.LlamaContext;
 import net.jllama.llama.cpp.java.bindings.LlamaOpaqueModel;
@@ -32,14 +33,10 @@ public class Main {
     final String pid = jvmName.split("@")[0];
     System.out.printf("pid=%s%n", pid);
   }
+  private static volatile String appLogLevel = System.getProperty("loglevel");
   private static LlamaCpp llamaCpp;
   private static LlamaOpaqueModel llamaOpaqueModel;
-<<<<<<< Updated upstream
-  private static LlamaOpaqueContext llamaOpaqueContext;
-  final private static Logger log = LogManager.getLogger(LlamaCpp.class);
-=======
   private static LlamaContext llamaOpaqueContext;
->>>>>>> Stashed changes
 
   public static void main(final String[] args) {
     try {
@@ -49,9 +46,20 @@ public class Main {
       llamaCpp.loadLibrary();
       llamaCpp.llamaBackendInit(true);
       llamaCpp.llamaLogSet((logLevel, message) -> {
+        final Logger log = LogManager.getLogger(llamaCpp.getClass());
         final String messageText = new String(message, StandardCharsets.UTF_8);
-        log.info(messageText);
+        if ("OFF".equalsIgnoreCase(appLogLevel)) {
+          return;
+        }
+        if (logLevel == LlamaLogLevel.INFO && "INFO".equalsIgnoreCase(appLogLevel)) {
+          log.info(messageText);
+        } else if (logLevel == LlamaLogLevel.WARN) {
+          log.warn(messageText);
+        } else {
+          log.error(messageText);
+        }
       });
+      long timestamp1 = llamaCpp.llamaTimeUs();
 
       final LlamaContextParams llamaContextParams = llamaCpp.llamaContextDefaultParams();
       final int threads = Runtime.getRuntime().availableProcessors() / 2 - 1;
@@ -59,16 +67,16 @@ public class Main {
       llamaContextParams.setnThreadsBatch(threads);
 
       final LlamaModelParams llamaModelParams = llamaCpp.llamaModelDefaultParams();
-      final long initStartUs = llamaCpp.llamaTimeUs();
       llamaOpaqueModel = llamaCpp.llamaLoadModelFromFile(
           modelPath.getBytes(StandardCharsets.UTF_8), llamaModelParams);
       llamaOpaqueContext =
           llamaCpp.llamaNewContextWithModel(llamaOpaqueModel, llamaContextParams);
-      final long initStopUs = llamaCpp.llamaTimeUs();
 
-      System.out.printf("timestamp1=%s, timestamp2=%s, initialization time=%s%n", initStartUs, initStopUs, initStartUs - initStopUs);
+      long timestamp2 = llamaCpp.llamaTimeUs();
 
-      final String prompt = B_INST + B_SYS + SYSTEM_PROMPT + E_SYS + "Explain Jonathan Joestar's special ability, \"Hamon.\"" + E_INST;
+      System.out.printf("timestamp1=%s, timestamp2=%s, initialization time=%s%n", timestamp1, timestamp2, timestamp2 - timestamp1);
+
+      final String prompt = B_INST + B_SYS + SYSTEM_PROMPT + E_SYS + "Suggest a Keto-friendly meal for dinner." + E_INST;
       final int[] tokens = tokenize(prompt, true);
 
       System.out.print(detokenizer.detokenize(toList(tokens), llamaOpaqueModel));
@@ -85,24 +93,13 @@ public class Main {
       final List<Integer> previousTokenList = new ArrayList<>();
       previousTokenList.add(previousToken);
 
-      final List<Long> evalTimestamps = new ArrayList<>();
-      final List<Long> logitsTimestamps = new ArrayList<>();
-      final List<Long> sampleTimestamps = new ArrayList<>();
-
       for (int i = tokens.length + 1; previousToken != llamaCpp.llamaTokenEos(llamaOpaqueContext) && i < llamaContextParams.getnCtx(); i++) {
-
-        long evalStartUs = llamaCpp.llamaTimeUs();
         final int res = llamaCpp.llamaEval(llamaOpaqueContext, new int[]{previousToken}, 1, i);
-        long evalStopUs = llamaCpp.llamaTimeUs();
-        evalTimestamps.add(evalStopUs - evalStartUs);
         if (res != 0) {
           throw new RuntimeException("Non zero response from eval");
         }
-        long logitsStartUs = llamaCpp.llamaTimeUs();
         logits = llamaCpp.llamaGetLogits(llamaOpaqueContext);
         candidates = LlamaTokenDataArray.logitsToTokenDataArray(logits);
-        long logitsStopUs = llamaCpp.llamaTimeUs();
-        logitsTimestamps.add(logitsStopUs - logitsStartUs);
 //        llamaCpp.llamaSampleRepetitionPenalty(llamaOpaqueContext, candidates, toArray(previousTokenList), 1.2f);
 //        llamaCpp.llamaSampleFrequencyAndPresencePenalties(llamaOpaqueContext, candidates, toArray(previousTokenList), -0.2f, -0.2f);
 //        llamaCpp.llamaSampleTopK(llamaOpaqueContext, candidates, 100, 1);
@@ -111,10 +108,7 @@ public class Main {
 //        llamaCpp.llamaSampleTailFree(llamaOpaqueContext, candidates, 0.5f, 1);
 //        llamaCpp.llamaSampleTypical(llamaOpaqueContext, candidates, 0.5f, 1);
         llamaCpp.llamaSampleTemperature(llamaOpaqueContext, candidates, temp);
-        long sampleStartUs = llamaCpp.llamaTimeUs();
         previousToken = llamaCpp.llamaSampleToken(llamaOpaqueContext, candidates);
-        long sampleStopUs = llamaCpp.llamaTimeUs();
-        sampleTimestamps.add(sampleStopUs - sampleStartUs);
         previousTokenList.add(previousToken);
         System.out.print(detokenizer.detokenize(previousToken, llamaOpaqueModel));
       }
@@ -123,22 +117,6 @@ public class Main {
       llamaCpp.llamaFreeModel(llamaOpaqueModel);
       llamaCpp.llamaBackendFree();
       llamaCpp.closeLibrary();
-      final double avgEvalTime = evalTimestamps.stream()
-          .mapToDouble(Long::doubleValue)
-          .average()
-          .orElseThrow(() -> new RuntimeException("average() returned no value"));
-      final double avgLogitsTime = logitsTimestamps.stream()
-          .mapToDouble(Long::doubleValue)
-          .average()
-          .orElseThrow(() -> new RuntimeException("average() returned no value"));
-      final double avgSampleTime = sampleTimestamps.stream()
-          .mapToDouble(Long::doubleValue)
-          .average()
-          .orElseThrow(() -> new RuntimeException("average() returned no value"));
-      System.out.println();
-      System.out.printf("averageEvalTime=%fms", avgEvalTime / 1000);
-      System.out.printf("averageLogitsTime=%fms", avgLogitsTime / 1000);
-      System.out.printf("averageSampleTime=%fms", avgSampleTime / 1000);
     } catch (RuntimeException e) {
       System.out.println("Fatal exception occurred, exceptionMessage=" + e.getMessage());
     }
